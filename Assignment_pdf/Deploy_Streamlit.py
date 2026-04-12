3b35704e084d4dcc25d8b321edf63f5e56a8264c
bharswami23 <76422348+bharswami23@users.noreply.github.com>
2026-04-12 20:57:56 +0530

    Update Deploy_Streamlit.py

--- Assignment_pdf/Deploy_Streamlit.py
+++ Assignment_pdf/Deploy_Streamlit.py
@@ -275,7 +275,7 @@ def main():
     
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     qa_path = os.path.join(BASE_DIR, "qa_list.json")
-    trans_model = "all_mini_lm"; #bge_large, bge_base, intfloat, all_mini_lm
+    trans_model = "bge_base"; #bge_large, bge_base, intfloat, all_mini_lm
     doc_path = os.path.join(BASE_DIR, f"documents_{trans_model}.json")
     floats_path = os.path.join(BASE_DIR, f"floats_{trans_model}.npy")
     model = models["all_mini_lm"]
