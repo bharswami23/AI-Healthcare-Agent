7b7598966565697aa29883276e003add2b70ae6b
bharswami23 <76422348+bharswami23@users.noreply.github.com>
2026-04-12 21:06:46 +0530

    Update Deploy_Streamlit.py

--- Assignment_pdf/Deploy_Streamlit.py
+++ Assignment_pdf/Deploy_Streamlit.py
@@ -25,11 +25,11 @@ def load_models():
     models["bge_base"] = SentenceTransformer(
         "BAAI/bge-base-en-v1.5", device="cpu"
     )
-    
+    """
     models["intfloat"] = SentenceTransformer(
         "intfloat/e5-base-v2", device="cpu"
     )
-    """
+    
     models["bge_large"] = SentenceTransformer(
         "BAAI/bge-large-en-v1.5", device="cpu"
     )
